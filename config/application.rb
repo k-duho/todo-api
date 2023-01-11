5023d5ab4fba2a10f841abce83e4220ec9cc4b26
duho Kim <dev.kimduho@gmail.com>
2023-01-12 01:04:39 +0900

    generator설정 변경

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,14 @@ module TodoApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      p g.methods
+      g.orm :active_record, migration: false
+      g.skip_routes     false
+      g.assets          false
+      g.helper          false
+      g.test_framework :test_unit, fixture: false, controller: false, model: false
+    end
   end
 end
