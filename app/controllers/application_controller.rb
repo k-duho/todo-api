20bc24da989b440c5aa4c64c0cf8a2d046fbfd68
duho Kim <dev.kimduho@gmail.com>
2023-01-17 23:45:54 +0900

    token유효기간이 5분 이내로 남았을 때 인증시 토큰유효기간을 자동 갱신 하도록 수정

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,22 @@
 class ApplicationController < ActionController::API
   def authenticate_user
-    unless current_user
-      render json: { error: 'Unauthorized' }, status: :unauthorized
+    unless current_user && current_user.token_expired_at > Time.current
+      render json: { error: 'Unauthorized' }, status: :unauthorized and return
+    end
+
+    if current_user.token_expired_at - Time.current > 2.minutes
+      refresh_token
     end
   end
 
   def current_user
     @current_user ||= User.find_by(token: request.headers['Authorization'])
+  end
+
+  private
+
+  def refresh_token
+    current_user.update(token: SecureRandom.hex,
+                        token_expired_at: Time.current + 30.minutes)
   end
 end
